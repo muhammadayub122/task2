6fe08b391a42e7c5f83949cd7c0038ad7e95575d
Твоё Имя <@example.com>
2026-04-27 17:21:37 +0500

    finish

--- demo_otp.py
+++ demo_otp.py
@@ -26,13 +26,13 @@ r2 = requests.post(BASE, json={
     "jsonrpc": "2.0",
     "method": "transfer.create",
     "params": {
-        "ext_id": "abc141",
-        "from_card": "9860010138878433",
-        "to_card": "1779912989738185",
-        "amount": 10000,
+        "ext_id": "abc1deen1",
+        "from_card": "8600 1234 5678 9012",
+        "to_card": "	9860 9876 5432 1098",
+        "amount": 900,
         "currency": 643,
-        "sender_card_expiry": "2027-04-01",
-        "chat_id": "5841388492",
+        "sender_card_expiry": "12/26",
+        "chat_id": "8643951926",
         "user_otp": otp
     },
     "id": 2
